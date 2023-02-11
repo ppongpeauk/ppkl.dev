fd67a14873bd3e10851503a5287de97ded8a02c9
Pete Pongpeauk <kurtsiberg@gmail.com>
2023-02-11 03:00:47 -0500

    draft design
    Signed-off-by: Pete Pongpeauk <kurtsiberg@gmail.com>

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,7 @@ import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from '../styles/Home.module.css'
 import Navbar from '../components/Navbar'
+import Link from 'next/link'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -11,16 +12,26 @@ export default function Home() {
     <>
       {/* <Navbar /> */}
       <main className={styles.main}>
-        <p>coming soon</p>
-        {/* <section className={styles.hero}>
-          <div className={styles.heroText}>
-            <h1 className={styles.heroTitle}>Pete Pongpeauk</h1>
-            <h3 className={styles.heroSubtitle}>computer science</h3>
+        <section className={styles.mainSection}>
+          {/* "about" hero */}
+          <div className={styles.heroRow}>
+            <div className={styles.heroText}>
+              <div>
+                <h1 className={styles.heroTitle}>Pete Pongpeauk</h1>
+                <p className={styles.heroSubtitle}>Computer Science student at George Mason University</p>
+              </div>
+              {/* links */}
+              <div className={styles.heroInnerRow}>
+                <a href="https://github.com/ppongpeauk" target="_blank" className={styles.homeLink}>GitHub</a>
+                <a href="https://www.linkedin.com/in/pete-pongpeauk/" target="_blank" className={styles.homeLink}>LinkedIn</a>
+                <a href="resume.pdf" target="_blank" className={styles.homeLink}>Résumé</a>
+              </div>
+            </div>
+            <div>
+              <Image className={styles.heroImage} src="/images/hero.png" alt="Hero" width={256} height={256} />
+            </div>
           </div>
-        </section> */}
-        {/* <footer className={styles.footer}>
-
-        </footer> */}
+        </section>
       </main>
     </>
   )
