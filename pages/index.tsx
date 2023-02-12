1b29f046362f65e661154564042eee06efb2a59b
Pete Pongpeauk <kurtsiberg@gmail.com>
2023-02-12 03:10:21 -0500

    logo experimentation
    Signed-off-by: Pete Pongpeauk <kurtsiberg@gmail.com>

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,13 +2,32 @@ import { Inter } from '@next/font/google'
 import Head from 'next/head'
 import Image from 'next/image'
 import Link from 'next/link'
+import { useRouter } from 'next/router'
 
 import Navbar from '../components/Navbar'
 import styles from '../styles/Home.module.css'
 
-import heroImage from './hero-image.png'
+import heroImage from '../assets/hero-image.png'
+import masonLogo from '../assets/mason-logo.png'
 
 const inter = Inter({ subsets: ['latin'] })
+
+interface HomeLinkProps {
+  href: string
+  children: React.ReactNode
+}
+
+function HomeLink({ href, children }: HomeLinkProps) {
+  const router = useRouter()
+  const className = router.pathname === href ? styles.homeLinkActive : ''
+
+  return (
+    <Link href={href} className={`${styles.homeLink} ${className}`}>
+      {children}
+    </Link>
+  )
+}
+
 
 export default function Home() {
   return (
@@ -34,14 +53,14 @@ export default function Home() {
             <div className={styles.heroText}>
               <div>
                 <h1 className={styles.heroTitle}>Pete Pongpeauk</h1>
-                <p className={styles.heroSubtitle}>Computer Science student at George Mason University</p>
+                <p className={styles.heroSubtitle}>Computer Science student at George Mason University <Image className={styles.intextLogo} src={masonLogo} alt="Mason Logo" width={16} height={16}></Image></p>
               </div>
               {/* links */}
               <div className={styles.heroInnerRow}>
-                <a href="https://github.com/ppongpeauk" target="_blank" rel="noreferrer" className={styles.homeLink}>GitHub</a>
-                <a href="https://www.linkedin.com/in/pete-pongpeauk/" target="_blank" rel="noreferrer" className={styles.homeLink}>LinkedIn</a>
-                <a href="mailto:ppongpeauk@gmail.com" target="_blank" rel="noreferrer" className={styles.homeLink}>Email</a>
-                <a href="resume.pdf" target="_blank" rel="noreferrer" className={styles.homeLink}>Résumé</a>
+                <a href="https://github.com/ppongpeauk" target="_blank" rel="noreferrer" className={styles.homeLinkExternal}>GitHub</a>
+                <a href="https://www.linkedin.com/in/pete-pongpeauk/" target="_blank" rel="noreferrer" className={styles.homeLinkExternal}>LinkedIn</a>
+                <a href="mailto:ppongpeauk@gmail.com" target="_blank" rel="noreferrer" className={styles.homeLinkExternal}>Email</a>
+                <a href="resume.pdf" target="_blank" rel="noreferrer" className={styles.homeLinkExternal}>Résumé</a>
               </div>
             </div>
             <div>
