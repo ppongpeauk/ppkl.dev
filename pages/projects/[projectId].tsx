818c8747c4a71a834cfe4301fffd41e11cdbd47f
Pete Pongpeauk <kurtsiberg@gmail.com>
2023-03-12 20:33:58 -0400

    add project page template
    Signed-off-by: Pete Pongpeauk <kurtsiberg@gmail.com>

---
+++ pages/projects/[projectId].tsx
@@ -0,0 +1,19 @@
+import { useRouter } from "next/router";
+
+export default function Project() {
+  const router = useRouter();
+  const { projectId } = router.query;
+  const projectData = {
+    num: 1,
+    name: "EVE",
+    description: "sample description"
+  }
+
+  return <>
+    <h1>
+      {/* add fancy leading zero if project number is below two digits long */}
+      {(projectData.num < 10) ? 0 : null}{projectData.num} - {projectData.name}
+    </h1>
+    <p>{projectData.description}</p>
+  </>
+}
